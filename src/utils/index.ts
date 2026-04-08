104f47e201575f6a6f8a33ef8da564744ed808fd
K4YZER4 <horachioxbox@gmail.com>
2026-04-08 15:30:15 -0700

    Generador de id con uuid

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,13 @@
 import { createFile, deleteFile, readFile, renameFile } from "./files.utils";
 import { logger } from "./logger.utils";
 import { executeCommand, currentDir } from "./bash.utils";
+import { generateUniqueId } from "./idGenerator.utils";
 export {
   createFile,
   deleteFile,
   readFile,
   renameFile,
+  generateUniqueId,
   logger,
   executeCommand,
   currentDir,
